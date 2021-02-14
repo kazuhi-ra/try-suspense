4df99da74aebc58937d02b8d0cd4b8fe09102bd8
kazuhi-ra <kazuhira4126@gmail.com>
2021-02-14 16:00:58 +0900

    できた

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sleep = (sec: number) =>
+  new Promise((resolve) => setTimeout(resolve, sec * 1000))
+
+type GreetType = 'yo' | 'hai' | 'hey'
+
+let cache: null | GreetType = null
+const lazyGreet = (greet: GreetType) => {
+  console.log(2, 6, 'lazyGreetが呼ばれました')
+  if (cache) {
+    console.log(7, 'cacheを返します')
+    return cache
+  }
+
+  const promise = sleep(1).then(() => {
+    console.log(4, 'promiseです。cacheします')
+    cache = greet
+  })
+
+  console.log(3, 'throwします')
+  throw promise
 }
 
-export default App;
+const Greet: React.VFC<{ greet: GreetType }> = ({ greet }) => {
+  console.log(1, 5, 'Greet()')
+  const yourGreet = lazyGreet(greet)
+  console.log('yourGreet', yourGreet)
+
+  return <div>{yourGreet}, KAZUHIRA!</div>
+}
+
+const App = () => {
+  console.log(0, 'App()')
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Greet greet='hai' />
+    </Suspense>
+  )
+}
+
+export default App
